a9149d53c65a4a1b3c9476ee6a1019cb5d740043
DineshTechCraft <dinesh.d2207.m@gmail.com>
2024-03-07 18:35:17 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import{useForm} from 'react-hook-form'
+/* eslint-disable no-undef */
 
-function App() {
+const App = () => {
+  const{register,handleSubmit,watch,formState:{errors}}=useForm();
+  const password=watch('password')
+
+  const onSubmit=(data)=>{
+    console.log(data);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="container d-flex justify-content-center mt-4 w-50">
+      
+      <div className='w-50'>
+      <h2 className=' text-primary'>REGISTRATION FORM</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label className="form-label fw-bold">First Name</label>
+          <input
+            type="text"
+            className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
+            {...register('firstName', { required: 'First Name is required' })}
+          />
+          {errors.firstName && <div className="invalid-feedback">{errors.firstName.message}</div>}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label fw-bold">Last Name</label>
+          <input
+            type="text"
+            className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+            {...register('lastName', { required: 'Last Name is required' })}
+          />
+          {errors.lastName && <div className="invalid-feedback">{errors.lastName.message}</div>}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label fw-bold">Email</label>
+          <input
+            type="email"
+            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+            {...register('email', { required: 'Email is required', pattern: /^\S+@\S+$/i })}
+          />
+          {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label fw-bold">Password</label>
+          <input
+            type="password"
+            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+            {...register('password', { required: 'Password is required', minLength:{value:6,message:'Password must be at least 6 characters'} })}
+          />
+          {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label fw-bold">Confirm Password</label>
+          <input
+            type="password"
+            className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
+            {...register('confirmPassword', {
+              required: 'Confirm Password is required',
+              validate: (value) => value === password|| 'Passwords does not match',
+            })}
+          />
+          {errors.confirmPassword && <div className="invalid-feedback">{errors.confirmPassword.message}</div>}
+        </div>
+        <div className='container  w-50 mx-auto text-center'>
+        <button type="submit" className="btn btn-primary ">Submit</button>
+        </div>
+      </form>
+      </div>
     </div>
-  );
+  
+  )
 }
 
-export default App;
+export default App
